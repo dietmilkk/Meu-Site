29486fc8024c8c52ce5ba53cf747a1145346c4af
dietmilkk <dietmilkk@users.noreply.github.com>
2026-06-09 12:00:04 -0300

    geo: switch from ip-api.com (SSL requires paid key) to ipapi.co (free HTTPS)

--- system/js/geo.js
+++ system/js/geo.js
@@ -2,23 +2,23 @@
   "use strict";
 
   global.fetchGeo = function() {
-    return fetch("https://ip-api.com/json/?fields=status,message,ip,city,region,country,postal,isp,org,lat,lon,timezone")
+    return fetch("https://ipapi.co/json/")
       .then(function(r) {
         if (!r.ok) return Promise.reject(new Error("HTTP " + r.status));
         return r.json();
       })
       .then(function(d) {
-        if (d.status === "fail") return Promise.reject(new Error(d.message || "geo failed"));
+        if (d.error) return Promise.reject(new Error(d.reason || "geo failed"));
         return {
           ip: d.ip,
           city: d.city,
           region: d.region,
-          country: d.country,
+          country: d.country_name,
           postal: d.postal,
-          isp: d.isp || d.org,
+          isp: d.org,
           org: d.org,
-          lat: d.lat,
-          lon: d.lon,
+          lat: d.latitude,
+          lon: d.longitude,
           timezone: d.timezone
         };
       });

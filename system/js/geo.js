1246862a2a5fb639f8207426a311da474daf64a0
dietmilkk <dietmilkk@users.noreply.github.com>
2026-06-09 13:13:05 -0300

    geo: switch to ipinfo.io (has CORS headers, free HTTPS)

--- system/js/geo.js
+++ system/js/geo.js
@@ -2,23 +2,24 @@
   "use strict";
 
   global.fetchGeo = function() {
-    return fetch("https://ipapi.co/json/")
+    return fetch("https://ipinfo.io/json")
       .then(function(r) {
         if (!r.ok) return Promise.reject(new Error("HTTP " + r.status));
         return r.json();
       })
       .then(function(d) {
         if (d.error) return Promise.reject(new Error(d.reason || "geo failed"));
+        var loc = (d.loc || "").split(",");
         return {
           ip: d.ip,
           city: d.city,
           region: d.region,
-          country: d.country_name,
+          country: d.country,
           postal: d.postal,
           isp: d.org,
           org: d.org,
-          lat: d.latitude,
-          lon: d.longitude,
+          lat: loc[0] ? parseFloat(loc[0]) : null,
+          lon: loc[1] ? parseFloat(loc[1]) : null,
           timezone: d.timezone
         };
       });

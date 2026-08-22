04778f0a1abfb422392e7f2b143ae1efb3e93d3c
dietmilkk <dietmilkk@users.noreply.github.com>
2026-08-21 21:35:46 -0300

    player: artista/tempo/controles em janela unica em todos tamanhos; artwork 1x1 ocupa resto vertical sem esmagar

--- apps/soundcloud/soundcloud.js
+++ apps/soundcloud/soundcloud.js
@@ -909,23 +909,26 @@
       if (large) {
         requestAnimationFrame(function() {
           var plWin = document.getElementById("scPlaylistWin");
-          var info = document.querySelector(".sc-info");
-          var controlsProgress = document.getElementById("scControlsProgress");
+          var metaWin = document.getElementById("scMetaWindow");
           var playerH = el.clientHeight;
           var otherH = 0;
           if (plWin) otherH += plWin.offsetHeight;
-          if (info) otherH += info.offsetHeight;
-          if (controlsProgress) otherH += controlsProgress.offsetHeight;
+          if (metaWin) otherH += metaWin.offsetHeight;
           else {
-            var progress = document.querySelector(".sc-progress-container");
-            var controls = document.querySelector(".sc-controls-row");
-            if (progress) otherH += progress.offsetHeight;
-            if (controls) otherH += controls.offsetHeight;
+            var info = document.querySelector(".sc-info");
+            var controlsProgress = document.getElementById("scControlsProgress");
+            if (info) otherH += info.offsetHeight;
+            if (controlsProgress) otherH += controlsProgress.offsetHeight;
+            else {
+              var progress = document.querySelector(".sc-progress-container");
+              var controls = document.querySelector(".sc-controls-row");
+              if (progress) otherH += progress.offsetHeight;
+              if (controls) otherH += controls.offsetHeight;
+            }
           }
           otherH += 16; // gaps/borders
           var available = playerH - otherH;
-          var size = Math.min(available, 280);
-          if (size < 120) size = 120;
+          var size = available;
           if (size < 0) size = 0;
           el.style.setProperty("--art-size", size + "px");
         });
